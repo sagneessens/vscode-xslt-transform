0121e270f0bf5b530583e2b02f48341b44354612
Sven Agneessens <sven.agneessens@gmail.com>
2019-11-10 13:35:24 +0100

    Fix #1: on data -> save in chunks array, on end -> write chunks to new file

--- src/xslt/xsltRunner.ts
+++ src/xslt/xsltRunner.ts
@@ -5,6 +5,7 @@ import {xsltOutputChannel} from "./xsltOutputChannel";
 export class Runner {
 
     private _process: ChildProcess | undefined;
+    private _chunks: Array<Buffer> = [];
 
     public runCommand(command: string, args: string[], data: string, cwd?: string) {
         xsltOutputChannel.clear();
@@ -12,31 +13,31 @@ export class Runner {
 
         this._process = spawn(command, args, {cwd: cwd, shell: true});
 
-        this._process.stdin.end(data, async () => {
+        this._process.stdin.end(data, () => {
             xsltOutputChannel.append("file contents written to stdin");
         });
 
-        this._process.stdout.on('data', async (data) => {
-            try {
-                const xmlDocument = await workspace.openTextDocument({
-                    content: data.toString(),
-                    language: "xml"
-                });
-                window.showTextDocument(xmlDocument, ViewColumn.Beside);
-            } catch(e) {
-                window.showErrorMessage("Failed to show output in a new document");
-                console.error(e);
-                xsltOutputChannel.append(data.toString());
-            }
+        this._process.stdout.on('data', (data) => {
+            this._chunks.push(data);
         });
 
         this._process.stderr.on('data', (data) => {
             xsltOutputChannel.append(data.toString());
         });
 
-        this._process.on("exit", (code) => {
+        this._process.on("exit", async (code) => {
             if (code === 0) {
                 window.showInformationMessage("XSLT transformation successfully executed.");
+                try {
+                    const xmlDocument = await workspace.openTextDocument({
+                        content: Buffer.concat(this._chunks).toString(),
+                        language: "xml"
+                    });
+                    window.showTextDocument(xmlDocument, ViewColumn.Beside);
+                } catch (e) {
+                    window.showErrorMessage("Failed to show output in a new document");
+                    console.error(e);
+                }
             } else if (code === 1) {
                 window.showInformationMessage("XSLT transformation failed.");
             }
